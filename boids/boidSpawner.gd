1af979c99877724c5ace13067c6df47e691f70a0
marwan <aitaddi.marwan@gmail.com>
2024-11-07 17:00:50 +0100

    Created project

---
+++ boids/boidSpawner.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export
+var boidNumber: int = 1000
+var boid_scene: PackedScene = preload("res://Scenes/boid.tscn")
+
+func _init() -> void:
+	for i in range(0, boidNumber):
+		var boid = boid_scene.instantiate()
+		boid.global_position.x = randf_range(0, 1920)
+		boid.global_position.y = randf_range(0, 1080)
+		
+		self.add_child(boid)
+	
+	
